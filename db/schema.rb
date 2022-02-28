01426e4c17a9367abc223398a2af4d2fca7d32d4
shadmanAh <shadman061@gmail.com>
2022-02-28 10:13:50 +0330

    add more fields to courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_27_062636) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_28_054748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_27_062636) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.string "slug"
+    t.text "short_description"
+    t.string "language", default: "English", null: false
+    t.string "level", default: "Beginner", null: false
+    t.integer "price", default: 0, null: false
     t.index ["slug"], name: "index_courses_on_slug", unique: true
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
@@ -87,6 +91,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_27_062636) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
