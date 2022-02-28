40b71e4023aebbe0cdd94201f3cef1b631057c26
shadmanAh <shadman061@gmail.com>
2022-02-28 18:01:41 +0330

    add gem public_activity

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :courses
   resources :users, only: [:index]
   get 'home/index'
+  get 'home/activity'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
