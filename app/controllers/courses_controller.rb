d9137a94723d8f2a95bc4ee3c56734e9dda0d13f
shadmanAh <shadman061@gmail.com>
2022-03-15 08:55:53 +0330

    validation who can see a course show

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -57,6 +57,7 @@ class CoursesController < ApplicationController
   end
 
   def show
+    authorize @course
     @lessons = @course.lessons
     @enrollments_with_review = @course.enrollments.reviewed
   end
--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -6,6 +6,13 @@ class CoursePolicy < ApplicationPolicy
     end
   end
 
+  def show?
+    @record.published && @record.approved || 
+    @user.present? && @user.has_role?(:admin) || 
+    @user.present? && @record.user_id == @user.id || 
+    @record.bought(@user) 
+  end
+
   def edit?
     @record.user_id == @user.id
   end
