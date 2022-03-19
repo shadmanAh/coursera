dae2f66c7d89035c7d71f44d3a5d1f3f3ea443b8
shadmanAh <shadman061@gmail.com>
2022-03-19 10:53:30 +0330

    video attechment and preview

--- app/javascript/controllers/trix-editor-overrides.js
+++ app/javascript/controllers/trix-editor-overrides.js
@@ -1,23 +1,23 @@
 // Blacklist all file attechments
-// window.addEventListener("trix-file-accept", function(event) {
-//   event.preventDefault()
-//   alert("File attachment not supported!")
-// })
+window.addEventListener("trix-file-accept", function(event) {
+  event.preventDefault()
+  alert("File attachment not supported!")
+})
 
 // Only images
-window.addEventListener("trix-file-accept", function(event) {
-  const acceptedTypes = ['image/jpeg', 'image/png']
-  if (!acceptedTypes.includes(event.file.type)){
-    event.preventDefault()
-    alert("Only support attachment of jpeg or png files")
-  }
-})
+// window.addEventListener("trix-file-accept", function(event) {
+//   const acceptedTypes = ['image/jpeg', 'image/png']
+//   if (!acceptedTypes.includes(event.file.type)){
+//     event.preventDefault()
+//     alert("Only support attachment of jpeg or png files")
+//   }
+// })
 
-// File size
-window.addEventListener("trix-file-accept", function(event) {
-  const maxFileSize = 1024 * 1024 // 1MB
-  if (event.file.size > maxFileSize){
-    event.preventDefault()
-    alert("Only support attachment files upto size 1MB!")
-  }
-})
+// // File size
+// window.addEventListener("trix-file-accept", function(event) {
+//   const maxFileSize = 1024 * 1024 // 1MB
+//   if (event.file.size > maxFileSize){
+//     event.preventDefault()
+//     alert("Only support attachment files upto size 1MB!")
+//   }
+// })
